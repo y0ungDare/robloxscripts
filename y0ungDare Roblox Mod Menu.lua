1194a64587ce1b25addfb55b3ab1c52c88d8d0af
y0ungDare <dareashjoshin7777.az@gmail.com>
2025-07-05 19:38:39 -0500

    Update y0ungDare Roblox Mod Menu.lua

--- y0ungDare Roblox Mod Menu.lua
+++ y0ungDare Roblox Mod Menu.lua
@@ -1,13 +1,3 @@
---[=[
- d888b  db    db d888888b      .d888b.      db      db    db  .d8b.  
-88' Y8b 88    88   `88'        VP  `8D      88      88    88 d8' `8b 
-88      88    88    88            odD'      88      88    88 88ooo88 
-88  ooo 88    88    88          .88'        88      88    88 88~~~88 
-88. ~8~ 88b  d88   .88.        j88.         88booo. 88b  d88 88   88    @uniquadev
- Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
-]=]
-
--- Instances: 34 | Scripts: 9 | Modules: 0 | Tags: 0
 local G2L = {};
 
 -- StarterGui.y0ungDare
